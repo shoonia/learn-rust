5e0d52056423ac08e1424d0d4cf7ac966de9ceba
shoonia <alexanderz@wix.com>
2026-02-08 23:51:44 +0200

    Implement sum function in WebAssembly module and update index.html for input handling

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,12 @@
+use std::slice::from_raw_parts;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn squarer(a: f64) -> f64 {
     a * a
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn sum(n: *const f64, len: usize) -> f64 {
+    let numbers = unsafe { from_raw_parts(n, len) };
+    numbers.iter().sum()
 }
