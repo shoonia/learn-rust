1c07bf72126d2f0c3619fd1d0c8661dfb872f937
shoonia <alexanderz@wix.com>
2026-02-12 22:52:40 +0200

    fix(routes): simplify not_found_route by removing unused OriginalUri extraction

--- crud-server/src/routes/not_found.rs
+++ crud-server/src/routes/not_found.rs
@@ -1,12 +1,11 @@
 use axum::{
     debug_handler,
-    extract::OriginalUri,
-    http::{Method, StatusCode},
+    http::{Method, StatusCode, Uri},
     response::IntoResponse,
 };
 
 #[debug_handler]
-pub async fn not_found_route(method: Method, OriginalUri(uri): OriginalUri) -> impl IntoResponse {
+pub async fn not_found_route(method: Method, uri: Uri) -> impl IntoResponse {
     (
         StatusCode::NOT_FOUND,
         format!("Route not found: {}: {}", method, uri),
