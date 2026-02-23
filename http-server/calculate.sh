2e63d359c644ee7d5822def7696e1d32cd4c93af
shoonia <alexanderz@wix.com>
2026-02-23 20:11:08 +0200

    Remove http-server module and associated files
    - Deleted Cargo.toml, calculate.sh, main.rs, model.rs, and routes.rs files from the http-server module.
    - This removal indicates the discontinuation of the HTTP server functionality.

--- http-server/calculate.sh
+++
@@ -1,9 +0,0 @@
-echo "\n"
-echo "Using GET request with query parameters:"
-
-curl "http://localhost:3000/calculate?a=10&b=20&operation=add"
-
-echo "\n"
-echo "Using POST request with JSON body:"
-
-curl -s "http://localhost:3000/calculate" -X POST -H "Content-Type: application/json" -d '{"a": 10, "b": 20, "operation": "add"}'
